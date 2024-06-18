062ce2f59fed1a22455f2617ed6bff72b49e7812
Valentina <valentina.bujic@teso.rs>
2024-06-18 11:06:39 +0200

    QS-88 edit ATSerialCommunication error messages, set read limit to 50

--- src/atSerialCommunication.cpp
+++ src/atSerialCommunication.cpp
@@ -13,28 +13,58 @@ ATSerialCommunication::ATSerialCommunication(const cable_t &cable, int verbose) 
     _ftdi = ftdi_new();
     if (_ftdi == NULL) {
         if (verbose != quiet)
-            printf("Unable to create ftdi\r\n");
+            printf("Error: Unable to create ftdi\r\n");
         return;
     }
     int interface = ftdi_set_interface(_ftdi, (ftdi_interface) cable.config.interface);
     if (interface < 0) {
-        if (verbose != quiet)
-            printf("Unable to set interface\r\n");
+        if (verbose != quiet) {
+            printf("Error: Unable to set interface: %d (%s)\n", interface,
+                   (interface == -1) ?
+                   "unknown interface"
+                                     : (interface == -2) ?
+                                       "USB device unavailable"
+                                                         : (interface == -3) ?
+                                                           "Device already open, interface can't be set in that state"
+                                                                             : "unknown error");
+        }
         return;
     }
 
     int ftStatus = ftdi_usb_open(_ftdi, cable.vid, cable.pid);
     if (ftStatus < 0) {
         if (verbose != quiet)
-            printf("Error opening usb device: %s\r\n", ftdi_get_error_string(_ftdi));
+            printf("Error: Can not open usb device: %s\r\n", ftdi_get_error_string(_ftdi));
         return;
     }
 
-    int latency = ftdi_set_latency_timer(_ftdi, ATSerialCommunication::LATENCY);
+    int latency_ret = ftdi_set_latency_timer(_ftdi, ATSerialCommunication::LATENCY);
+    if (latency_ret < 0) {
+        if (verbose != quiet) {
+            printf("Error: Can not set latency timer: %d (%s)\n", latency_ret,
+                   (latency_ret == -1) ?
+                   "latency out of range (Value should be between 1 and 255)"
+                                       : (latency_ret == -2) ?
+                                         "unable to set latency timer"
+                                                             : (latency_ret == -3) ?
+                                                               "USB device unavailable"
+                                                                                   : "unknown error");
+        }
+    }
 
-    if (ftdi_set_baudrate(_ftdi, ATSerialCommunication::BAUDRATE) < 0) {
-        if (verbose != quiet)
-            printf("Error setting baud rate! \n");
+    int baudrate_ret = ftdi_set_baudrate(_ftdi, ATSerialCommunication::BAUDRATE);
+    if (baudrate_ret < 0) {
+        if (verbose != quiet) {
+
+            printf("Error: Can not set baud rate: %d (%s)\n", baudrate_ret,
+                   (baudrate_ret == -1) ?
+                   "invalid baudrate"
+                                        : (baudrate_ret == -2) ?
+                                          "setting baudrate failed"
+                                                               : (baudrate_ret == -3) ?
+                                                                 "USB device unavailable"
+                                                                                      : "unknown error");
+        }
         return;
     }
 }
@@ -49,9 +79,17 @@ std::string ATSerialCommunication::write_command(unsigned char *command, int len
         printf("Sending: %s\n", command);
 
     int ret = ftdi_write_data(_ftdi, command, len);
-    if (ret != len) {
+    if (ret < 0) {
+        if (verbose != quiet) {
+            printf("Error: Failed to write data: %d (%s)\n", ret,
+                   (ret == -666) ?
+                   "USB device unavailable"
+                                 : "error code from usb_bulk_write()");
+        }
+        return "NULL";
+    } else if (ret != len) {
         if (verbose != quiet)
-            printf("Write error: %d (written %d)\n", ret, len);
+            printf("Error: Failed to write data: Only %d out of %d bytes are written\n", ret, len);
         return "NULL";
     }
 
@@ -60,15 +98,21 @@ std::string ATSerialCommunication::write_command(unsigned char *command, int len
     ret = ftdi_read_data(_ftdi, u_response, ATSerialCommunication::MESSAGE_SIZE);
 
     int tries = 0;
-    while (ret == 0) {
+    while (ret == 0 && tries < 50) {
         if (verbose != quiet)
-            printf("Got empty response. Reading again! %d\n", ++tries);
+            printf("Warning: Got empty response. Reading again! %d\n", ++tries);
         ret = ftdi_read_data(_ftdi, u_response, ATSerialCommunication::MESSAGE_SIZE);
     }
 
     if (ret <= 0) {
-        if (verbose != quiet)
-            printf("Read error: %s\n", std::to_string(ret).c_str());
+        if (verbose != quiet) {
+            printf("Error: Failed to read data: %d (%s)\n", ret,
+                   (ret == 0) ?
+                   "no data was available"
+                              : (ret == -666) ?
+                                "USB device unavailable"
+                                              : "error code from libusb_bulk_transfer()");
+        }
         return "NULL";
     }
 
